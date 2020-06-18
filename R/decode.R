830ecccec20b9530395ac22a045fa234c1d406f1
Merlin Unterfinger <info@munterfinger.ch>
2020-06-18 09:21:25 +0200

    [pkg] Add type parsing of third dimension in decode().

--- R/decode.R
+++ R/decode.R
@@ -29,15 +29,36 @@ decode <- function(encoded, crs = 4326) UseMethod("decode", encoded)
 
 #' @export
 decode.character <- function(encoded, crs = 4326) {
+
   dim3 <- character(length(encoded))
+  ind3 <- 2
+  sfdi <- "XY"
+
   geom <- sf::st_sfc(
     lapply(1:length(encoded), function(x) {
       m <- decode_coords(encoded[[x]])
-      dim3[x] <<- colnames(m)[3]
-      sf::st_linestring(m)
+      d3 <- colnames(m)[3]
+      if (is.na(d3)) {
+        dim3[x] <<- "ABSENT"
+        ind3 <<- 2
+        sfdi <<- "XY"
+      } else {
+        dim3[x] <<- d3
+        ind3 <<- 3
+        if (d3 %in% c("LEVEL", "ALTITUDE", "ELEVATION")) {
+          message(d3)
+          sfdi <<- "XYZ"
+        } else {
+          sfdi <<- "XYM"
+        }
+      }
+      sf::st_linestring(m, dim = sfdi)
     }),
-    crs = crs)
+    crs = crs
+  )
+
   dim3[is.na(dim3)] <- "ABSENT"
+
   return(
     sf::st_as_sf(
       data.frame(
