4cb0e4358f9931114237b77e4a8fad8c3b193666
Merlin Unterfinger <info@munterfinger.ch>
2020-11-25 13:39:51 +0100

    [ci] Update tests.

--- tests/testthat/test-decode_sf.R
+++ tests/testthat/test-decode_sf.R
@@ -2,24 +2,25 @@ test_that("decode_sf works", {
 
   # Encoded lines
   n <- 5
-  encodedXY <- "BFoz5xJ67i1B1B7PzIhaxL7Y"
-  encodedXYZ <- "BlBoz5xJ67i1BU1B7PUzIhaUxL7YU"
-  encodedXYM <- as.factor("BlXoz5xJ67i1Bgkh9B1B7Pgkh9BzIhagkh9BxL7Ygkh9B")
+  encodedXYZ <- "B1Voz5xJ67i1Bgkh9B"
+  encodedXY <- "BFoz5xJ67i1B1B7PlU9yB"
+  encodedXYM <- as.factor("BlXoz5xJ67i1Bgkh9B1B7Pgkh9BzIhagkh9BqK-pB_ni6D")
 
   # Decode
+  pointXYZ <- decode_sf(rep(encodedXYZ, n))
   lineXY <- decode_sf(rep(encodedXY, n))
-  lineXYZ <- decode_sf(rep(encodedXYZ, n))
-  lineXYM <- decode_sf(rep(encodedXYM, n))
+  polyXYM <- decode_sf(rep(encodedXYM, n))
 
   # Test decode_sf()
+  expect_s3_class(pointXYZ, c("sf", "data.frame"), exact = TRUE)
   expect_s3_class(lineXY, c("sf", "data.frame"), exact = TRUE)
-  expect_s3_class(lineXYZ, c("sf", "data.frame"), exact = TRUE)
-  expect_s3_class(lineXYM, c("sf", "data.frame"), exact = TRUE)
+  expect_s3_class(polyXYM, c("sf", "data.frame"), exact = TRUE)
+  expect_s3_class(decode_sf(c("BlXoz5xJ67i1Bgkh9B", "BlXoz5xJ67i1Bgkh9B1B7Pgkh9BzIhagkh9B", "BlXoz5xJ67i1Bgkh9B1B7Pgkh9BzIhagkh9BqK-pB_ni6D")), c("sf", "data.frame"), exact = TRUE)
+  expect_true(all(sf::st_geometry_type(pointXYZ) == "POINT"))
   expect_true(all(sf::st_geometry_type(lineXY) == "LINESTRING"))
-  expect_true(all(sf::st_geometry_type(lineXYZ) == "LINESTRING"))
-  expect_true(all(sf::st_geometry_type(lineXYM) == "LINESTRING"))
+  expect_true(all(sf::st_geometry_type(polyXYM) == "POLYGON"))
+  expect_equal(nrow(pointXYZ), n)
   expect_equal(nrow(lineXY), n)
-  expect_equal(nrow(lineXYZ), n)
-  expect_equal(nrow(lineXYM), n)
+  expect_equal(nrow(polyXYM), n)
 
 })
--- tests/testthat/test-encode_sf.R
+++ tests/testthat/test-encode_sf.R
@@ -1,47 +1,39 @@
 test_that("encode_sf works", {
 
-  # 2d line
-  line2d <- matrix(
-    c(8.69821, 50.10228,
-      8.69567, 50.10201,
-      8.69150, 50.10063,
-      8.68752, 50.09878),
-    ncol = 2, byrow = TRUE
-  )
+  # 3D point
+  point3d <- sf::st_point(
+    matrix(c(8.69821, 50.10228, 10), ncol = 3, byrow = TRUE), dim = "XYZ")
 
-  # 3d line
-  line3d <- matrix(
-    c(8.69821, 50.10228, 10.11111,
-      8.69567, 50.10201, 20.22222,
-      8.69150, 50.10063, 30.33333,
-      8.68752, 50.09878, 40.44444),
-    ncol = 3, byrow = TRUE
-  )
+  # 2D linestring
+  line2d <- sf::st_linestring(
+    matrix(c(8.69821, 50.10228,
+             8.69567, 50.10201,
+             8.68752, 50.09878), ncol = 2, byrow = TRUE))
+
+  # 3D polygon
+  poly3d <- sf::st_polygon(list(
+    matrix(c(8.69821, 50.10228, 10,
+             8.69567, 50.10201, 20,
+             8.69150, 50.10063, 30,
+             8.69821, 50.10228, 10), ncol = 3, byrow = TRUE)), dim = "XYM")
 
   # Test encode_sf()
-  expect_error(
-    encode_sf(sf::st_point(c(1,2,3))),
-    "Invalid geometry type 'POINT' of input, only 'LINESTRING' is supported."
-  )
-
+  expect_error(encode_sf(sf::st_multilinestring()),
+               "Invalid geometry type 'MULTILINESTRING' of input, only 'POINT', 'LINESTRING' and 'POLYGON' is supported.")
   ## sfg (XY, XYZ and XYM)
-  line2d_sfg <- sf::st_linestring(line2d)
-  line3d_sfg <- sf::st_linestring(line3d, dim = "XYZ")
-  line3dm_sfg <- sf::st_linestring(line3d, dim = "XYM")
-  expect_type(encode_sf(line2d_sfg), "character")
-  expect_type(encode_sf(line3d_sfg), "character")
-  expect_type(encode_sf(line3dm_sfg), "character")
-
+  expect_type(encode_sf(point3d), "character")
+  expect_type(encode_sf(line2d), "character")
+  expect_type(encode_sf(poly3d), "character")
   ## sfc (XY and XYZ)
-  line2d_sfc <- sf::st_as_sfc(list(line2d_sfg, line2d_sfg), crs = 4326)
-  line3d_sfc <- sf::st_as_sfc(list(line3d_sfg, line3d_sfg), crs = 4326)
+  point3d_sfc <- sf::st_as_sfc(list(point3d, point3d), crs = 4326)
+  line2d_sfc <- sf::st_as_sfc(list(line2d, line2d), crs = 4326)
+  poly3d_sfc <- sf::st_as_sfc(list(poly3d, poly3d), crs = 4326)
+  expect_type(encode_sf(point3d_sfc), "character")
   expect_type(encode_sf(line2d_sfc), "character")
-  expect_type(encode_sf(line3d_sfc), "character")
-
+  expect_type(encode_sf(poly3d_sfc), "character")
   ## sf (XY and XYZ)
-  line2d_sf <- sf::st_as_sf(line2d_sfc)
-  line3d_sf <- sf::st_as_sf(line3d_sfc)
-  expect_type(encode_sf(line2d_sf), "character")
-  expect_type(encode_sf(line3d_sf), "character")
+  expect_type(encode_sf(sf::st_as_sf(point3d_sfc)), "character")
+  expect_type(encode_sf(sf::st_as_sf(line2d_sfc)), "character")
+  expect_type(encode_sf(sf::st_as_sf(poly3d_sfc)), "character")
 
 })
